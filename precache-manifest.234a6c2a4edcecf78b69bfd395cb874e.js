46e957b3aa2e63ddf0e58544c708436859a60d7c
bofice <chimpbofice@gmail.com>
2020-09-15 17:54:17 +0530

    react app added
    added app

---
+++ precache-manifest.234a6c2a4edcecf78b69bfd395cb874e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c153def58a233351bb87f96fbf7a90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "538978e5ccf05b2aae2d",
+    "url": "/static/css/main.8d4e92ac.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538978e5ccf05b2aae2d",
+    "url": "/static/js/main.a5e0dbee.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
